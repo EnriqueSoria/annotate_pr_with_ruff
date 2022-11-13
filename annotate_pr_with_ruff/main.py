fbea0a880fd01ee04894ec87a9c6e80df63a62cd
Enrique Soria <me@enriquesoria.com>
2022-11-13 12:56:01 +0100

    remove filtering lines changed per file as it's not needed

--- annotate_pr_with_ruff/main.py
+++ annotate_pr_with_ruff/main.py
@@ -15,22 +15,18 @@ def main():
     owner, repo = event_data["pull_request"]["head"]["repo"]["full_name"].split("/")
     pr_number = event_data["pull_request"]["number"]
 
+    # Check which files have changed
     diff = get_diff(owner=owner, repo=repo, pr_number=pr_number)
-
     changed_files = get_changed_files(diff)
 
+    # Get errors from ruff
     ruff_errors = ruff()
-    locations = {"/" + ruff_error.file.removeprefix("/") for ruff_error in ruff_errors}
-    changed_files = {
-        filename.removeprefix("/"): lines.intersection()
-        for filename, lines in changed_files.items()
-        if filename in locations
-    }
 
+    # Filter only errors on lines that have been changed
     ruff_errors = [
         ruff_error
         for ruff_error in ruff_errors
-        if ruff_error.line_number in changed_files.get(ruff_error.file, {})
+        if ruff_error.line_number in changed_files.get(f"/{ruff_error.file}", {})
     ]
 
     submit_review(owner=owner, repo=repo, pr_number=pr_number, errors=ruff_errors)
